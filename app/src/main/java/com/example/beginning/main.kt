df2b492cd34af91cec3dec92332a6ba04b882907
Anshu <anshukumarsingh448@gmail.com>
2023-07-16 14:34:29 +0530

    Initial commit

---
+++ app/src/main/java/com/example/beginning/main.kt
@@ -0,0 +1,7 @@
+fun main(){
+    val myCharValue: Char='0'
+    val myBooleanValue: Boolean = false
+
+   // println(myCharValue)
+    println("myCharValue = $myCharValue and myBooleanValue = $myBooleanValue")
+}
