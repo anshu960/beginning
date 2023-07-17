a38e57693354356238f02ab1329b4dd29c57bfef
Anshu <anshukumarsingh448@gmail.com>
2023-07-17 16:38:46 +0530

    begins

--- app/src/main/java/com/example/beginning/function.kt
+++ app/src/main/java/com/example/beginning/function.kt
@@ -1,10 +1,23 @@
 fun main(){
-   sayHello()
-    sayHello("Anshu Kumar",29)
-    sayHello("Vivek",30)
-}
-
-fun sayHello(name: String = "Nisha",age: Int = 24){
-   // var number = age
-    println("Hello, $name, $age")
+//   sayHello()
+//    sayHello("Anshu Kumar",29)
+//    sayHello("Vivek",30)
+//    val hasInternetConnection = true
+//    if (!hasInternetConnection){
+//        getData("Some data")
+//    }else{
+//        showMessage()
+//    }
+//}
+//
+//fun showMessage() {
+//    println("There's no internet connect")
+//}
+//
+//fun sayHello(name: String = "Nisha",age: Int = 24){
+//   // var number = age
+//    println("Hello, $name, $age")
+//}
+//fun getData(data: String){
+//    println("Your data is: $data")
 }
