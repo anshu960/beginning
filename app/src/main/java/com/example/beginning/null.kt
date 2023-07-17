a14a442a6b517974a1916d88016a4fa0a8fdd41b
Anshu <anshukumarsingh448@gmail.com>
2023-07-17 17:40:19 +0530

    begins

--- app/src/main/java/com/example/beginning/null.kt
+++ app/src/main/java/com/example/beginning/null.kt
@@ -12,9 +12,15 @@ fun main() {
 //    var text: String? = null
 //    text = "Some text"
 //    println(text!!.length)
-    var text: String? = null
-    text = "This variable is not null"
-    val text2 = text ?: "Some text."
-    println(text2)
-
+//    var text: String? = null
+//    text = "This variable is not null"
+//    val text2 = text ?: "Some text."
+ //   println(text2)
+    sendMessage("Anshu")
+}
+fun sendMessage(name: String="Hello", message: String =whatsapp()){
+    println("Name = $name and Message = $message")
+}
+fun whatsapp(): String{
+    return "Some Text"
 }
